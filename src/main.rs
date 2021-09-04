660d286cc43d56f3b22d318d1a27af8ca53d3ba6
HuyHa <huyha14082012@gmail.com>
2021-09-04 13:57:26 +0700

    developing the library's functionality with test driven development

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,9 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
     }
 }
-
